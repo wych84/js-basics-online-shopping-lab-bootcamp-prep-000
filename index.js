18281c133189b977802bbc3643f65b13db4ad984
Joyce Chun <wooyangee@gmail.com>
2018-04-26 01:42:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,22 +9,33 @@ function setCart(c) {
   return cart;
 }
 
+
 function addToCart(item) {
- // write your code here
+  cart.push({itemName: item, itemPrice: Math.floor(Math.random() * 100) + 1}); 
+  return `${item} has been added to your cart.`;
 }
 
-function viewCart() {
-  // write your code here
-}
 
 function total() {
-  // write your code here
+  var sum = 0;
+  for (var i = 0; i < cart.length; i++) {
+  sum += cart[i].itemPrice;
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (item == cart[i].itemName)
+    cart.splice(cart[i].itemName,1);
+}
+return cart;
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+}
+
+function viewCart() {
+
 }
